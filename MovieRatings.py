a7c6c085db7fc2ab11f33767cd6c963086702106
renadalahmadi <85293746+renadalahmadi@users.noreply.github.com>
2022-11-25 21:27:52 +0300

    Add files via upload

--- MovieRatings.py
+++ MovieRatings.py
@@ -6,15 +6,20 @@ class RatingsBreakdown(MRJob):
     def steps(self):
         return [
             MRStep(mapper=self.mapper_get_ratings,
-                   reducer=self.reducer_count_ratings)
+                   reducer=self.reducer_count_ratings),
+            MRStep(reducer=self.reducer_sorted_output)
         ]
 
     def mapper_get_ratings(self, _, line):
-        (userID, movieID, rating, timestamp) = line.split('::')
-        yield rating, 1
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield movieID, 1
 
     def reducer_count_ratings(self, key, values):
-        yield key, sum(values)
+        yield str(sum(values)).zfill(5), key
+        
+    def reducer_sorted_output(self, count, movies):
+        for movie in movies:
+            yield movie, count
 
 if __name__ == '__main__':
     RatingsBreakdown.run()
