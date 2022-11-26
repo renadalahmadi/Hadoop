a0cd75e93911ea1cd86ad56d36381f39f3072ccc
renadalahmadi <85293746+renadalahmadi@users.noreply.github.com>
2022-11-26 15:16:08 +0300

    Update MedicalDevices.py

--- MedicalDevices.py
+++ MedicalDevices.py
@@ -1,4 +1,3 @@
-# importing MRJob and MRStep from mrjob package 
 from mrjob.job import MRJob
 from mrjob.job import MRStep
 
@@ -11,12 +10,12 @@ class MedicalDevices(MRJob):
     ]
     
 def mapper_get_ratings(self, _, line):
-  (tradeName, genericName, IssueDate, ExpiryDate, Manufacture_CountryEn,IsLocalManufacturer,Jurisdiction_en, deviceType_en ,Classification_en,accessories) = line.split(',')
-   yield rating, 1
+    (tradeName, genericName, IssueDate, ExpiryDate, Manufacture_CountryEn,IsLocalManufacturer,Jurisdiction_en, deviceType_en ,Classification_en,accessories) = line.split(',')
+    yield rating, 1
 
     
 def reducer_count_ratings(self, key, values):
-   yield key, sum(values)
+    yield key, sum(values)
     
     
 if __name__ == '__main__':
