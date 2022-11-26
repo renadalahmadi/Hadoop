21c51cb5fc0436df5f902d1ae070eb8b8ae62d95
renadalahmadi <85293746+renadalahmadi@users.noreply.github.com>
2022-11-26 14:38:39 +0300

    Add files via upload

---
+++ MedicalDevices.py
@@ -0,0 +1,24 @@
+# importing MRJob and MRStep from mrjob package
+from mrjob.job import MRJob
+from mrjob.job import MRStep
+
+class MedicalDevices(MRJob):
+    # Define single mapreduce phase
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_device_countries,
+            reducer=self.reducer_count_ratings)
+    ]
+
+def mapper_device_countries(self, _, line):
+   (tradeName, genericName, IssueDate, ExpiryDate, Manufacture_CountryEn,IsLocalManufacturer
+   ,Jurisdiction_en, deviceType_en ,Classification_en,accessories) = line.split(',')
+    yield Manufacture_CountryEn, 1
+
+def reducer_count_countries(self, key, values):
+    yield key, sum(values)
+    
+    
+if __name__ == '__main__':
+    MedicalDevices.run()
+
