8324f57de6e4954c86f8f32c0bb7183aa2da12bf
renadalahmadi <85293746+renadalahmadi@users.noreply.github.com>
2022-11-26 15:10:56 +0300

    Update MedicalDevices.py

--- MedicalDevices.py
+++ MedicalDevices.py
@@ -1,23 +1,23 @@
-# importing MRJob and MRStep from mrjob package
+# importing MRJob and MRStep from mrjob package 
 from mrjob.job import MRJob
 from mrjob.job import MRStep
 
+
 class MedicalDevices(MRJob):
-    # Define single mapreduce phase
     def steps(self):
         return [
-            MRStep(mapper=self.mapper_device_countries,
-            reducer=self.reducer_count_countries)
+            MRStep(mapper=self.mapper_get_ratings,
+            reducer=self.reducer_count_ratings)
     ]
+    
+def mapper_get_ratings(self, _, line):
+  (tradeName, genericName, IssueDate, ExpiryDate, Manufacture_CountryEn,IsLocalManufacturer,Jurisdiction_en, deviceType_en ,Classification_en,accessories) = line.split(',')
+    yield rating, 1
 
-def mapper_device_countries(self, _, line):
-    (tradeName, genericName, IssueDate, ExpiryDate, Manufacture_CountryEn,IsLocalManufacturer,Jurisdiction_en, deviceType_en ,Classification_en,accessories) = line.split(',')
-    yield movieID , rating
-
-def reducer_count_countries(self, key, values):
+    
+def reducer_count_ratings(self, key, values):
     yield key, sum(values)
     
     
 if __name__ == '__main__':
     MedicalDevices.run()
-
