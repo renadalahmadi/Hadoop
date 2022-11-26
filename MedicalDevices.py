30b498a962409d7397e978f50264bf2da6c9db84
renadalahmadi <85293746+renadalahmadi@users.noreply.github.com>
2022-11-26 16:04:41 +0300

    Update MedicalDevices.py

--- MedicalDevices.py
+++ MedicalDevices.py
@@ -1,22 +1,30 @@
 from mrjob.job import MRJob
-from mrjob.job import MRStep
+from mrjob.step import MRStep
+import csv
 
+#split by ,
+columns = 'tradeName,genericName,IssueDate,ExpiryDate,Manufacture_CountryEn,IsLocalManufacturer,Jurisdiction_en,deviceType_en,Classification_en,accessories'.split(',')
 
 class MedicalDevices(MRJob):
-    def steps(self):
-        return [
-            MRStep(mapper=self.mapper_get_ratings,
-            reducer=self.reducer_count_ratings)
-    ]
-    
-def mapper_get_ratings(self, _, line):
-    (tradeName, genericName, IssueDate, ExpiryDate, Manufacture_CountryEn,IsLocalManufacturer,Jurisdiction_en, deviceType_en ,Classification_en,accessories) = line.split(',')
-    yield rating, 1
+ def steps(self):
+     return[
+        MRStep(mapper=self.mapper_get_ratings,
+              reducer=self.reducer_count_ratings)
+        ]
+#Mapper function 
+ def mapper_get_ratings(self, _, line):
+       reader = csv.reader([line])
+       for row in reader:
+           zipped=zip(columns,row)
+           diction=dict(zipped)
+           distc = diction['Manufacture_CountryEn']
+           #outputing as key value pairs
+           yield distc, 1
+           
+#Reducer function
+ def reducer_count_ratings(self, key, values):
+       yield key, sum(values)
 
-    
-def reducer_count_ratings(self, key, values):
-    yield key, sum(values)
-    
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     MedicalDevices.run()
